8e3c58ace1d17faeae78a9f4e9542380473e9194
= <elijahmuraoka03@gmail.com>
2023-02-18 14:44:05 -0500

    add mobile menu v2

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 // A single menu item link
 export default function MenuItem({ link, value, mobile = false }) {
-    const menuClasses = mobile ? 'font-bold px-6' : '';
+    const menuClasses = mobile ? 'font-bold px-8' : '';
     const menuLink = (
         <a href={link} className={`${menuClasses} hover:text-darkGrayishBlue`}>
             {value}
